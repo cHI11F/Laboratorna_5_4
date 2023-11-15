b51135eac03b387580869c1ba3b0b921d00c63f9
cHI11F <nazar.vata@dspu.edu.ua>
2023-11-15 16:27:05 +0200

    initial commit

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,41 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "..//PR18/PR18.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+    TEST_CLASS(UnitTest1)
+    {
+    public:
+
+        TEST_METHOD(TestMethod1)
+        {
+
+            int K = 2;
+            int N = 6;
+            double expected = 0.0;
+            for (int i = K; i <= N; i++) {
+                expected += (sin(cos(i))) / (1 + pow(cos(i), 2));
+            }
+
+
+            Assert::AreEqual(S0(K, N), expected);
+
+            int i = K;
+            Assert::AreEqual(S1(K, N, i), expected);
+
+            i = N;
+            Assert::AreEqual(S2(K, N, i), expected);
+
+            i = K;
+            double t = 0.0;
+            Assert::AreEqual(S3(K, N, i, t), expected);
+
+            i = N;
+            t = 0.0;
+            Assert::AreEqual(S4(K, N, i, t), expected);
+        }
+    };
+}
